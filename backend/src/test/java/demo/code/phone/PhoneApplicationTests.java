9d73a2974efd80066f6b9f99644ef25a625f6c30
J <justin.mccune@gmail.com>
2020-04-27 21:00:27 -0400

    Spring Initializr project

---
+++ backend/src/test/java/demo/code/phone/PhoneApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.code.phone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
